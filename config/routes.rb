fd25eccebf22282732ad379c2ec3558b6fdd0727
Adam Scott <ascot21@gmail.com>
2012-11-11 12:43:09 -0600

    layout tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Devbookclub::Application.routes.draw do
+  get   '/login', :to => 'sessions#new', :as => :login
+  match '/auth/:provider/callback', :to => 'sessions#create'
+  match '/auth/failure', :to => 'sessions#failure'
+  get '/logout', :to => 'sessions#destroy'
+
   root :to => 'home#index'
 
   resources :books do
