fbf9017bf4efce7614d4f74de79e39e1ecf5b409
Adam Scott <ascot21@gmail.com>
2012-11-15 14:37:41 -0600

    link to book admin in dashboard and site title goes to root

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,7 +1,9 @@
 ActiveAdmin::Dashboards.build do
   section "Recent Books" do
       table_for Book.order("created_at desc").limit(5) do
-          column :title
+          column :title do |book|
+            link_to book.title, admin_book_path(book)
+          end
           column :created_at
       end
       strong { link_to "View All Books", admin_books_path}
