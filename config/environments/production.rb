1e24aa94fc5bcaa9695f627d20277011caf30bd5
Adam Scott <ascot21@gmail.com>
2012-11-20 22:41:34 -0600

    use gmail for emails instead of sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,8 +52,20 @@ Devbookclub::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   #Setup production mailer
-  config.action_mailer.default_url_options = { :host => 'devbookclub.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'devbookclubl.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "devbookclub.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
   # Enable threaded mode
   # config.threadsafe!
